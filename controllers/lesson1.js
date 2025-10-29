756d75ed2bd443aa9404034c816e01a0d83b7654
ola466@gmail.com <ola466@gmail.com>
2025-10-29 03:23:31 -0500

    add controllers lesson 1 assignment

---
+++ controllers/lesson1.js
@@ -0,0 +1,13 @@
+const emilyRoute = (req, res) => {
+    res.send("Emily Birch")
+};
+
+const hannahRoute = (req, res) => {
+    res.send("Hanna Birch")
+};
+
+
+module.exports = {
+    emilyRoute,
+    hannahRoute
+};
